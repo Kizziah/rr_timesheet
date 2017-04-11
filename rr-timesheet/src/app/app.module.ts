5ca39e8c44d29c21f537a2e2172e77fdd6e60437
kizziah <mkizziah38@gmail.com>
2017-04-11 16:47:15 -0400

    posit client installed

--- rr-timesheet/src/app/app.module.ts
+++ rr-timesheet/src/app/app.module.ts
@@ -1,20 +1,62 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { LoginComponent }     from './login.component';
+import { AccountComponent }     from './account.component';
+import { HomeComponent }     from './home.component';
+import { UserComponent }     from './user.component';
+import { ProfileComponent }     from './profile.component';
+import { ContractComponent }     from './contract.component';
+import { CalendarsComponent }     from './calendars.component';
+import { LaborRatesComponent }     from './labor-rates.component';
+import { TimesheetsComponent }     from './timesheets.component';
+import { TimeOffComponent }     from './time-off.component';
+import { ExpensesComponent }     from './expenses.component';
+import { PTOComponent }     from './pto.component';
+import { UserPreferencesComponent }     from './user-preferences.component';
+import { SystemRolesComponent }     from './system-roles.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule,
+                  FormsModule,
+                  HttpModule,
+                  RouterModule.forRoot([
+                    { path: '', redirectTo: '/home', pathMatch: 'full' },
+                    { path: 'app', component: AppComponent },
+                    { path: 'login', component: LoginComponent}, 
+                    { path: 'account', component: AccountComponent },
+                    { path: 'home', component: HomeComponent },
+                    { path: 'user', component: UserComponent },
+                    { path: 'profile', component: ProfileComponent },
+                    { path: 'contract', component: ContractComponent },
+                    { path: 'calendars', component: CalendarsComponent },
+                    { path: 'labor-rates', component: LaborRatesComponent },
+                    { path: 'timesheets', component: TimesheetsComponent},
+                    { path: 'time-off', component: TimeOffComponent},
+                    { path: 'expenses', component: ExpensesComponent},
+                    { path: 'pto', component: PTOComponent},
+                    { path: 'user-preferences', component: UserPreferencesComponent},
+                    { path: 'system-roles', component: SystemRolesComponent}  
+                  ])],
+  declarations: [ AppComponent,
+                  LoginComponent,
+                  AccountComponent,
+                  HomeComponent,
+                  UserComponent,
+                  ProfileComponent,
+                  ContractComponent,
+                  CalendarsComponent,
+                  LaborRatesComponent,
+                  TimesheetsComponent,
+                  TimeOffComponent,
+                  ExpensesComponent,
+                  PTOComponent,
+                  UserPreferencesComponent, 
+                  SystemRolesComponent],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ rr-timesheet/src/app/system-roles.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'system-roles',
+  moduleId: module.id,
+  styleUrls: ['./all.component.css'],
+  templateUrl: './system-roles.component.html',
+
+})
+
+export class SystemRolesComponent  { /*name = 'PostIt';*/
+    
+}
